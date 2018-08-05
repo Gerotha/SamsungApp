f5494ee1e83bfa90ef8f0ae7f13b0d4546068de9
Gerotha <prp.paulo97@gmail.com>
2018-08-04 23:03:43 -0300

    firebase packages add

--- app/src/main/java/com/insightvalley/samsungapp/MainActivity.java
+++ app/src/main/java/com/insightvalley/samsungapp/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme_Launcher);
+        setTheme(R.style.SplashTheme);
         super.onCreate(savedInstanceState);
         setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
